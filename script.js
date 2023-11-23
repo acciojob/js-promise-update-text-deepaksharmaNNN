ba8a99564dacd2b448f2502f9a65627bfaee0b4f
Deepak Sharma <dsharma2828@gmail.com>
2023-11-23 11:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function updateTextWithPromise() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+function updateElementText() {
+  const outputElement = document.getElementById('output');
+
+  updateTextWithPromise()
+    .then(result => {
+      outputElement.innerText = result;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+// Call the function to update the element text after the promise resolves
+updateElementText();
